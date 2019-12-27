3cb9c9cadf1267ff0afd6c7145904121093e3c54
Alexander Johnston <Aliics@hotmail.com>
2019-12-27 23:21:25 +1300

    Add param handling to handler for range

--- src/main/java/fish/eyebrow/blog/backend/handler/BlogFetchHandler.java
+++ src/main/java/fish/eyebrow/blog/backend/handler/BlogFetchHandler.java
@@ -1,12 +1,41 @@
 package fish.eyebrow.blog.backend.handler;
 
 import io.vertx.core.Handler;
+import io.vertx.core.MultiMap;
 import io.vertx.ext.web.RoutingContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Objects;
 
 public class BlogFetchHandler implements Handler<RoutingContext> {
 
+    private static Logger LOGGER = LoggerFactory.getLogger(BlogFetchHandler.class);
+
+
     @Override
     public void handle(RoutingContext event) {
-        event.response().end("{}");
+        MultiMap params = event.queryParams();
+
+        try {
+            int start = parseIntNonNullOrElse(params.get("start"), Integer.MIN_VALUE);
+            int end = parseIntNonNullOrElse(params.get("end"), Integer.MAX_VALUE);
+
+            if (end < start) {
+                event.response().setStatusCode(400).end();
+                return;
+            }
+
+            event.response().end("{}");
+        }
+        catch (NumberFormatException e) {
+            LOGGER.error("Could not parse start and end params as integers");
+            event.response().setStatusCode(400).end();
+        }
+    }
+
+
+    private int parseIntNonNullOrElse(String param, int other) {
+        return Objects.nonNull(param) ? Integer.parseInt(param) : other;
     }
 }
--- src/test/java/fish/eyebrow/blog/backend/handler/BlogFetchHandlerTestCase.java
+++ src/test/java/fish/eyebrow/blog/backend/handler/BlogFetchHandlerTestCase.java
@@ -1,6 +1,8 @@
 package fish.eyebrow.blog.backend.handler;
 
+import static io.restassured.RestAssured.given;
 import static io.restassured.RestAssured.when;
+import static org.hamcrest.Matchers.emptyString;
 import static org.hamcrest.Matchers.equalTo;
 
 import com.google.inject.Guice;
@@ -13,6 +15,8 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 class BlogFetchHandlerTestCase {
+
+    public static final String PATH = "http://localhost:8080/blog";
 
     private Vertx vertx;
 
@@ -33,8 +37,34 @@ class BlogFetchHandlerTestCase {
     @Test
     void shouldRespondWithEmptyJsonWhenNoPostsExist() {
         when()
-                .get("http://localhost:8080/blog")
+                .get(PATH)
                 .then()
                 .body(equalTo(TestUtil.readFile("empty_response.json")));
+    }
+
+
+    @Test
+    void shouldRespondWith400WhenGivenInverseRange() {
+        given()
+                .param("start", 0)
+                .param("end", -1)
+                .when()
+                .get(PATH)
+                .then()
+                .statusCode(400)
+                .body(emptyString());
+    }
+
+
+    @Test
+    void shouldRespondWith400WhenGivenBadStart() {
+        given()
+                .param("start", "bad")
+                .param("end", 0)
+                .when()
+                .get(PATH)
+                .then()
+                .statusCode(400)
+                .body(emptyString());
     }
 }
